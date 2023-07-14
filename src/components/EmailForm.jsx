c4d641deed24a94b61573aa48e68ab319a1f8a14
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2023-07-14 21:11:20 +0700

    contact-us
    application ở be cấu hình lại mail và test

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Grid, TextField, Button, Card, CardContent, Typography } from '@material-ui/core';
 
 const EmailForm = () => {
-  const [to, setTo] = useState('');
-  const [subject, setSubject] = useState('');
-  const [content, setContent] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const data = {
-      to,
-      subject,
-      content,
+      to: email,
+      subject: `Message from ${firstName} ${lastName}`,
+      content: `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nPhone: ${phone}\n\n${message}`,
     };
 
     try {
@@ -25,35 +28,91 @@ const EmailForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="to">To:</label>
-      <input
-        type="email"
-        id="to"
-        value={to}
-        onChange={(e) => setTo(e.target.value)}
-        required
-      /><br /><br />
-
-      <label htmlFor="subject">Subject:</label>
-      <input
-        type="text"
-        id="subject"
-        value={subject}
-        onChange={(e) => setSubject(e.target.value)}
-        required
-      /><br /><br />
-
-      <label htmlFor="content">Content:</label>
-      <textarea
-        id="content"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        required
-      ></textarea><br /><br />
-
-      <button type="submit">Send</button>
-    </form>
+    <div className="App">
+      <Typography gutterBottom variant="h3" align="center">
+        Learning-Hub
+      </Typography>
+      <Grid>
+        <Card style={{ maxWidth: 450, padding: '20px 5px', margin: '0 auto' }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              Contact Us
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+              Fill up the form and our team will get back to you within 24 hours.
+            </Typography>
+            <form onSubmit={handleSubmit}>
+              <Grid container spacing={1}>
+                <Grid xs={12} sm={6} item>
+                  <TextField
+                    placeholder="Enter first name"
+                    label="First Name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </Grid>
+                <Grid xs={12} sm={6} item>
+                  <TextField
+                    placeholder="Enter last name"
+                    label="Last Name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="email"
+                    placeholder="Enter email"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    type="number"
+                    placeholder="Enter phone number"
+                    label="Phone"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Message"
+                    multiline
+                    rows={4}
+                    placeholder="Type your message here"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" color="primary" fullWidth>
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </div>
   );
 };
 
